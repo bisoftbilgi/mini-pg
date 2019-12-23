3410f611842f435405c967756b515b5388a4f168
kubra bicak <kubra.bicak89@gmail.com>
2019-12-23 10:55:32 +0300

    basebackup command is executed but data file is still missing

--- src/main/java/com/bisoft/minipg/service/pgwireprotocol/server/PgBaseBackup.java
+++ src/main/java/com/bisoft/minipg/service/pgwireprotocol/server/PgBaseBackup.java
@@ -5,22 +5,25 @@ import com.bisoft.minipg.service.pgwireprotocol.server.Response.CommandExecutor;
 import com.bisoft.minipg.service.pgwireprotocol.server.Response.Table;
 import com.bisoft.minipg.service.pgwireprotocol.server.Response.TableHelper;
 import com.bisoft.minipg.service.subservice.ConfigurationService;
+
 import java.io.File;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PgBaseBackup extends AbstractWireProtocolPacket {
 
-    private static final String PG_BASEBACKUP     = "-- pg_basebackup";
-    private static final String MASTER_IP         = "(?<masterIp>.*)";
+    private static final String PG_BASEBACKUP = "-- pg_basebackup";
+    private static final String MASTER_IP = "(?<masterIp>.*)";
     private static final String RIGHT_PARANTHESIS = "[)]";
-    private static final String LEFT_PARANTHESIS  = "[(]";
+    private static final String LEFT_PARANTHESIS = "[(]";
     String BASE_BACKUP = ".*" + PG_BASEBACKUP + LEFT_PARANTHESIS + MASTER_IP + RIGHT_PARANTHESIS + ".*";
     public String IpOfMaster;
+//    public String IpOfTarget;
 
     public WireProtocolPacket decode(byte[] buffer) {
 
@@ -28,26 +31,23 @@ public class PgBaseBackup extends AbstractWireProtocolPacket {
         Matcher m = p.matcher(getPayloadString());
         m.matches();
         IpOfMaster = m.group("masterIp");
+//        IpOfTarget = m.group("targetIp");
         return this;
     }
 
     @Override
     public byte[] response() {
 
-        Table table     = null;
-        File  parentDir = new File(ConfigurationService.GetValue("minipg.postgres_data_path"));
-        if (parentDir.isDirectory() && parentDir.list() != null && parentDir.list().length == 0) {
-            log.info("minipg.postgres_data_path is empty");
-            String basebackupCommand = ConfigurationService.GetValue("minipg.postgres_bin_path") + "pg_basebackup" + "-h"
-                + "--source-server=\"host=" + IpOfMaster + "\"" + "-D" + ConfigurationService.GetValue("minipg.postgres_data_path");
-            log.info("EXECUTING THIS COMMAND for basebackup===> " + basebackupCommand);
-            List<String> cellValues = (new CommandExecutor()).executeCommand(
-                ConfigurationService.GetValue("minipg.postgres_bin_path") + "pg_basebackup",
-                "--target-pgdata=" + ConfigurationService.GetValue("minipg.postgres_data_path"),
-                "--source-server=\"host=" + IpOfMaster + "\"");
-            cellValues.add(0, PG_BASEBACKUP + " received.." + basebackupCommand + " command executed at : " + new Date());
-            table = (new TableHelper()).generateSingleColumnTable("result", cellValues, "SELECT");
-        }
+        Table table = null;
+
+        String basebackupCommand = ConfigurationService.GetValue("minipg.postgres_bin_path") + "pg_basebackup " + "-h "
+                + "--source-server=\"host=" + IpOfMaster + "\"" + " -D " + ConfigurationService.GetValue("minipg.postgres_data_path");
+        log.info("EXECUTING THIS COMMAND for basebackup===> " + basebackupCommand);
+        List<String> cellValues = (new CommandExecutor()).executeCommand(
+                ConfigurationService.GetValue("minipg.postgres_bin_path") + "pg_basebackup" , "-h " ,
+                "--source-server=\"host=" + IpOfMaster + "\"" , "-D " + ConfigurationService.GetValue("minipg.postgres_data_path"));
+        cellValues.add(0, PG_BASEBACKUP + " received.." + basebackupCommand + " command executed at : " + new Date());
+        table = (new TableHelper()).generateSingleColumnTable("result", cellValues, "SELECT");
         return table != null ? table.generateMessage() : null;
     }
 
---
+++ src/test/java/com/bisoft/minipg/MiniPgTest.java
@@ -0,0 +1,101 @@
+package com.bisoft.minipg;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MiniPgTest {
+
+    public static void main(String[] args) throws Exception {
+
+        MiniPgTest m = new MiniPgTest();
+        m.stopMe();
+        m.closeMyConn();
+    }
+
+    private Connection conn = null;
+
+    private Connection getConnection() {
+
+        // String url =
+        String url = "jdbc:postgresql://192.168.2.133:9998/postgres?user=postgres&password=080419";
+//        String url = System.getenv("POSTGRES_JDBC_URL");
+        System.out.println("connecting to :" + url);
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    private void stopMe() throws Exception {
+
+//        String command = "-- pg_remove_sync_slave(server7)";
+          String command = "-- pg_basebackup(192.168.2.134)";
+//        String command = "-- pg_rewind(192.168.2.134)";
+//        String command  = "-- pg_start";
+        this.conn = getConnection();
+        Statement statement = conn.createStatement();
+        ResultSet rs        = statement.executeQuery(command);
+        while (rs.next()) {
+            String cell = rs.getString("result");
+            System.out.println(cell);
+        }
+
+    }
+//    public Connection getConnection(String IP, String port, String username, String dbname, String password)
+//            throws Exception {
+//
+//        connection = null;
+//
+//        System.out
+//                .println("IP::::" + IP + "dbname::::" + dbname + "username::::" + username + "password::::" + password);
+//
+//        connection = DriverManager.getConnection("jdbc:postgresql://" + IP + ":" + port + "/" + dbname, username,
+//                password);
+//
+//        return connection;
+//    }
+
+    public boolean closeMyConn() {
+
+        try {
+            conn.close();
+            System.out.println("Connection closed");
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+//
+//    public String testMe() {
+//
+//        String ip     = "localhost";
+//        String passwd = "080419";
+//
+//        String     statusOfMine = null;
+//        Connection conn         = null;
+//        try {
+////            conn = this.getConnection(ip, "9998", "postgres", "postgres", passwd);
+//            conn = this.getConnection();
+//            Statement statement = conn.createStatement();
+//
+//            ResultSet rs = statement.executeQuery("-- pg_start");
+//            if (rs.next()) {
+//                statusOfMine = statusOfMine + rs.getString("result");
+//                System.out.println(statusOfMine + " IP:" + ip);
+//            }
+//        } catch (Exception ex) {
+////                  Logger.getLogger(PostgresService.class.getName()).log(Level.SEVERE, null, ex);
+//            System.out.println("ERROR during thestarting of the PG process:{}" + ex.getMessage());
+//            return null;
+//        } finally {
+//            this.closeMyConn();
+//        }
+//        return statusOfMine;
+//    }
+
+}
