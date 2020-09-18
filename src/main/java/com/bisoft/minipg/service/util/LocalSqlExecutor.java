b787a94547f5c388c08ddfb202020cfd2519c2c6
Yasar <ysrgozudeli@gmail.com>
2020-09-18 12:58:07 +0300

    pseudo code for

--- src/main/java/com/bisoft/minipg/service/util/LocalSqlExecutor.java
+++ src/main/java/com/bisoft/minipg/service/util/LocalSqlExecutor.java
@@ -12,23 +12,23 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class LocalSqlExecutor {
-    
+
     private static final String LOCAL_IP = "127.0.0.1";
-    
+
     public List<String> retrieveLocalSqlResult(String sqlString, String localPort, String localUser,
-            String localPassword) {
-        
+                                               String localPassword) {
+
         System.out.println("sql executing:" + sqlString);
         log.trace("sql executing:" + sqlString);
-        
+
         List<String> cellValues = new ArrayList<>();
-        
+
         try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:" + localPort + "/postgres",
-                localUser, localPassword)) {
+            localUser, localPassword)) {
             Statement stmt = conn.createStatement();
-            
+
             Class.forName("org.postgresql.Driver");
-            
+
             ResultSet result = stmt.executeQuery(sqlString);
             String    line;
             while (result.next()) {
@@ -37,38 +37,33 @@ public class LocalSqlExecutor {
                 log.trace(line);
                 cellValues.add(line);
             }
-        }
-        
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
-            
+
         }
-        
+
         return cellValues;
     }
-    
+
     public void executeLocalSql(String sqlString, String localPort, String localUser, String localPassword) {
-        
+
         System.out.println("sql executing:" + sqlString);
         log.trace("sql executing:" + sqlString);
-        
+
         try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:" + localPort + "/postgres",
-                localUser, localPassword)) {
+            localUser, localPassword)) {
             Statement stmt = conn.createStatement();
-            
+
             Class.forName("org.postgresql.Driver");
-            
+
             stmt.executeUpdate(sqlString);
-            
-        }
-        
-        catch (Exception e) {
+
+        } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
-            
         }
-        
+
     }
-    
+
 }
