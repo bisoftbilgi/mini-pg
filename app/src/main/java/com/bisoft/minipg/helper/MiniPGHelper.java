4c93e22bd441aa5052438accd5940d487f5e3c1b
sdtbsmc <sedatbasmaci@gmail.com>
2024-07-01 17:50:21 +0000

    pg_ctl start witout -w param

--- app/src/main/java/com/bisoft/minipg/helper/MiniPGHelper.java
+++ app/src/main/java/com/bisoft/minipg/helper/MiniPGHelper.java
@@ -201,9 +201,8 @@ public class MiniPGHelper {
         List<String> result_start1 = (new ScriptExecutor()).executeScript(
                     miniPGlocalSetings.getPgCtlBinPath() + "pg_ctl",
                     "start",
-                    "-w",
-                    "-D", 
-                    miniPGlocalSetings.getPostgresDataPath());
+                    "-D " + miniPGlocalSetings.getPostgresDataPath());
+
         log.info("pg_start result : " + result_start1.toString());
 
         if ((result_start1.toString()).contains("error") || (result_start1.toString()).contains("fatal")){
@@ -241,10 +240,10 @@ public class MiniPGHelper {
         List<String> result_start = (new ScriptExecutor()).executeScript(
             miniPGlocalSetings.getPgCtlBinPath() + "pg_ctl",
             "start",
-            "-w",
-            "-D", 
-            miniPGlocalSetings.getPostgresDataPath());
-            log.info("pg_start result : " + result_start.toString());
+            "-D " + miniPGlocalSetings.getPostgresDataPath());
+
+        log.info("pg_start result : " + result_start.toString());
+        
         if ((result_start.toString()).contains("error") || (result_start.toString()).contains("fatal")){
             log.info(" Error occurrred on START PG, error:"+result_start.toString());
             return result_start.toString();
